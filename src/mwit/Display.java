ceeeab7ca11a8bd1b31d0fea04a87e5caf589eb6
mwittstrom <magnus@stickerapp.com>
2017-06-20 09:20:05 +0200

    Random

--- src/mwit/Display.java
+++ src/mwit/Display.java
@@ -28,14 +28,17 @@ public class Display {
 
         // create SPI object instance for SPI for communication
         // DEFAULT_SPI_SPEED = 1 MHz
-        this.spi = SpiFactory.getInstance(SpiChannel.CS0, SpiDevice.DEFAULT_SPI_SPEED, SpiMode.MODE_0);
+        this.spi = SpiFactory.getInstance(SpiChannel.CS0, 4000000, SpiMode.MODE_0);
 
         initializeLeds();
     }
 
     private void initializeLeds(){
         //TODO read from config
-        leds.add(new Led(31, 0, 0, 0, 0, 0));
+        int nofLeds = 50;
+        for (int i = 0; i < nofLeds; i++){
+            leds.add(new Led(31, 0, 0, 0, 0, 0));
+        }
     }
 
     public void updateLeds() throws IOException {
--- src/mwit/Main.java
+++ src/mwit/Main.java
@@ -24,7 +24,7 @@ public class Main {
     public static void main(String[] args) throws InterruptedException, IOException {
 
         Main main = new Main();
-        main.startApi();
+        //main.startApi();
         main.loop();
     }
 
@@ -33,7 +33,51 @@ public class Main {
             //Do stuff
 
             try {
-                display.updateLeds();
+                int random = (int)(Math.random() * 50);
+                int color = (int)(Math.random() * 3);
+                if (color == 0){
+                    int r = display.leds.get(random).r;
+                    if (r == 0){
+                        for (int i = 0; i < 256; i++){
+                            display.leds.get(random).r = i;
+                            display.updateLeds();
+                        }
+                    } else {
+                        for (int i = 255; i >= 0 ; i--){
+                            display.leds.get(random).r = i;
+                            display.updateLeds();
+                        }
+                    }
+                }
+                if (color == 1){
+                    int r = display.leds.get(random).g;
+                    if (r == 0){
+                        for (int i = 0; i < 256; i++){
+                            display.leds.get(random).g = i;
+                            display.updateLeds();
+                        }
+                    } else {
+                        for (int i = 255; i >= 0 ; i--){
+                            display.leds.get(random).g = i;
+                            display.updateLeds();
+                        }
+                    }
+                }
+                if (color == 2){
+                    int r = display.leds.get(random).b;
+                    if (r == 0){
+                        for (int i = 0; i < 256; i++){
+                            display.leds.get(random).b = i;
+                            display.updateLeds();
+                        }
+                    } else {
+                        for (int i = 255; i >= 0 ; i--){
+                            display.leds.get(random).b = i;
+                            display.updateLeds();
+                        }
+                    }
+                }
+                //display.updateLeds();
             }
             catch (IOException e) {
                 e.printStackTrace();
@@ -42,16 +86,16 @@ public class Main {
         }
     }
 
-    private void startApi(){
-        try {
-            HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
-            server.createContext("/api", new Api(display));
-            server.setExecutor(null); // creates a default executor
-            server.start();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+//    private void startApi(){
+//        try {
+//            HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
+//            server.createContext("/api", new Api(display));
+//            server.setExecutor(null); // creates a default executor
+//            server.start();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//    }
 
     public static String bytesToString(byte[] bytes){
 
